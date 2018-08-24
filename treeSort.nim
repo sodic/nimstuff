584280c7af0c564fb9406e110a05290c1a9eb7a1
Filip Sodić <filip.sodic@fer.hr>
2018-08-24 23:10:10 +0800

    add preorder iterator

--- treeSort.nim
+++ treeSort.nim
@@ -1,6 +1,6 @@
 import os
-import strutils
 import sequtils
+import strutils
 
 type Node = 
   ref object
@@ -22,29 +22,6 @@ proc fullNode(element: int): Node =
     right: emptyNode() 
   )  
 
-proc inorder(tree: Node): seq[int] =
-  proc inorderR (node: Node, acc: var seq[int]) : void =
-    if node.empty: 
-      return
-
-    node.left.inorderR(acc)
-    acc.add(node.value)
-    node.right.inorderR(acc)
-  
-  tree.inorderR(result)
-
-# no recursive iterators yet
-iterator preorder(node: Node): int =
-  var stack : seq[Node] = @[node]
-  while stack.len > 0:
-    var current = stack.pop()
-    while not current.empty:
-      yield current.value
-      stack.add(current.right)
-      current = current.left
-
-# TODO: implement inorder iterator
-
 proc add(tree: var Node, element: int) : void =
   if tree.empty:
     tree = fullNode(element)
@@ -52,13 +29,38 @@ proc add(tree: var Node, element: int) : void =
 
   if element < tree.value:
     tree.left.add(element)
+
   elif element > tree.value:
     tree.right.add(element)
 
+# no recursive iterators yet
+iterator preorder(node: Node): int =
+  var stack : seq[Node] = @[node]
+  
+  while stack.len > 0:
+    var current = stack.pop()
+    
+    while not current.empty:
+      yield current.value
+      stack.add(current.right)
+      current = current.left
+
+iterator inorder(node: Node): int = 
+  var current = node
+  var stack: seq[Node] = @[]
+  
+  while stack.len > 0 or not current.empty:
+    while not current.empty:
+      stack.add(current)
+      current = current.left
+
+    current = stack.pop()
+    yield current.value
+    current = current.right
 
 var t = emptyNode()
 for e in stdin.readLine().split().map(parseInt):
   t.add(e)
 
-for e in t.preorder():
-  echo e
+for el in t.inorder():
+  echo el
